a001455373577012d38aece0f60ce3c825c1f268
Dipanitha D <112413347+Dipanitha09@users.noreply.github.com>
2023-04-03 06:51:23 +0000

    dynamic html and css

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask,render_template 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data Analysis",
+    'location': "New York, USA",
+    'salary': '1000000'
+  },
+  {
+    'id': 2,
+    'title': "Data Engineer",
+    'location': "Delhi, India",
+    'salary': '1500000'
+  },
+  {
+    'id': 3,
+    'title': "Data Miner",
+    'location': "Banglore, India",
+    'salary': '1200000'
+  },
+  {
+    'id': 4,
+    'title': "Fashion Designer",
+    'location': "New York, USA",
+    'salary': '1800000'
+  },
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="ZYRA")
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug = True)
+  app.run(host='0.0.0.0', debug=True)
